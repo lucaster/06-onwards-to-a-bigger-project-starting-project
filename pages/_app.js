efcb84c818b094a8e43504bfe54b11f30843516f
lucaster <luca.cavagnoli@gmail.com>
2022-01-26 16:14:35 +0100

    added components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
+/**
+ * This will decorate all page components.
+ * Add common layout here
+ */
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <h1>WRAPPED</h1>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
